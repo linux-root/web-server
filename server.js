24d906ca23318b26de755df49e1f9e45a34b4621
Kurro <superusr.root@gmail.com>
2017-01-02 10:16:37 +0700

    update router

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var middleware = {
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
-app.get('/about',middleware.requireAuthentication, function(req, res){
+app.get('/private/route',middleware.requireAuthentication, function(req, res){
   res.send('superusr: mr.kurro');
 });
 app.use(express.static(__dirname+'/public'));
